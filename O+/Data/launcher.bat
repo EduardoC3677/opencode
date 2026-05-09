7a167d32759e69f9adc3e868f3239a87b41c894a
EduardoC3677 <ealvarado2677@gmail.com>
2026-05-08 18:16:35 -0600

    m

--- O+/Data/launcher.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Launcher - runs after extraction with UAC elevation
-
-REM Launch ServerManager with admin rights
-powershell -WindowStyle Hidden -Command "Start-Process '%TEMP%\ServerManager\ServerManager.exe' -ArgumentList '/silent','/autostart' -Verb RunAs"
-
-exit
